47acc1cc92049cc4b996fa70c505833cfb4576e8
jack <hongmomanu@163.com>
2015-03-27 13:56:58 +0800

    jack commit

---
+++ app/view/Contacts.js
@@ -0,0 +1,26 @@
+Ext.define('DoctorApp.view.Contacts', {
+    extend: 'Ext.List',
+    xtype: 'contacts',
+
+    config: {
+        title: '',
+        cls: 'x-contacts',
+        variableHeights: true,
+        //refreshHeightOnUpdate :false,
+        scrollToTopOnRefresh :false,
+
+        store: 'Contacts',
+        items: [{
+            xtype: 'button',
+            scrollDock: 'bottom',
+            docked: 'bottom',
+            itemId:'loadmorelist',
+            text: 'Load More...'
+        }],
+        itemTpl: [
+            '<div class="headshot"></div>',
+            '{firstName} {lastName}',
+            '<span>{title}</span>'
+        ].join('')
+    }
+});
