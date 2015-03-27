e510fb8076e2625273ca6ed5a5e33a8f3ac34e29
jack <hongmomanu@163.com>
2015-03-27 11:36:33 +0800

    jack commit

--- app.js
+++ app.js
@@ -44,6 +44,7 @@ Ext.application({
         Ext.fly('appLoadingIndicator').destroy();
 
         // Initialize the main view
+        //alert(11);
         Ext.Viewport.add(Ext.create('DoctorApp.view.Main'));
     },
 
